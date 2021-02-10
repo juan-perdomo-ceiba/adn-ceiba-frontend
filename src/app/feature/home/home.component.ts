6ab1e086a70cb5bc0a999dbef583fbd7b967ea77
Juan Jose Perdomo Forero <juan.perdomo@ceiba.com.co>
2021-02-10 12:57:32 -0500

    se agragan carga de paquetes turisticos en home para realizar reserva

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PaqueteTuristico } from '@paquete-turistico/shared/model/paquete-turistico';
+import { PaqueteTuristicoService } from '@paquete-turistico/shared/service/paquete-turistico.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,11 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public $paquetesTuristicos: Observable<PaqueteTuristico[]>;
 
 
-  constructor() { }
+  constructor(protected paqueteTuristicoServices: PaqueteTuristicoService) { }
 
   ngOnInit() {
+    this.$paquetesTuristicos = this.paqueteTuristicoServices.consultar();
   }
 
 }
